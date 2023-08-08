55d2c6afb151dcf584055729fbfeeab1b97b3e82
Lord Ghapa <felipef32@hotmail.com>
2023-08-07 21:10:18 -0300

    create component post card

---
+++ src/components/PostCard/index.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+import * as Styled from './styles';
+import { Heading } from '../Heading';
+import { Cover } from '../../shared-types/Cover';
+
+export type PostCardProps = {
+  slug: string;
+  resumo: string;
+  covered: string;
+  title: string;
+  cover: Cover;
+};
+
+export const PostCard = ({ title, cover, resumo, slug }: PostCardProps) => {
+  return (
+    <Styled.Wrapper>
+      <Link href={`/post/${slug}`}>
+        <Styled.Cover
+          src={cover.data.attributes.url}
+          alt={cover.data.attributes.altText}
+        />
+      </Link>
+
+      <Heading as="h2" size="small">
+        <Link href={`/post/${slug}`}>{title}</Link>
+      </Heading>
+
+      <Styled.Resumo>{resumo}</Styled.Resumo>
+    </Styled.Wrapper>
+  );
+};
---
+++ src/components/PostCard/styles.ts
@@ -0,0 +1,36 @@
+import styled, { css } from 'styled-components';
+import { Title as HeadingStyles } from '../Heading/styles';
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    ${HeadingStyles} {
+      margin: 0;
+      margin-block:1rem;
+    }
+
+    a {
+      text-decoration: none;
+      color: inherit;
+      transition: all 300ms ease-in-out;
+    }
+
+    &:hover a {
+      color: ${theme.colors.secondary};
+    }
+
+    &:hover img {
+      opacity: 0.8;
+    }
+  `}
+`;
+
+export const Cover = styled.img`
+  max-width: 100%;
+  transition: opacity 300ms ease-in-out;
+  aspect-ratio: 3/2;
+`;
+
+export const Resumo = styled.p`
+font-size: 1.6rem;
+margin: 0;
+`;
