a9410438a898ee03c763a9992dbc552ae21de3de
Lord Ghapa <felipef32@hotmail.com>
2023-08-15 18:55:11 -0300

    create logic to redirect page to home in 3s if page not found

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,26 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
 export default function notFount() {
-  return <h1>PAGINA NÃO ENCONTRADA</h1>;
+  const [time, setTime] = useState(3);
+  const router = useRouter();
+  useEffect(() => {
+    const changePageTime = setInterval(function () {
+      setTime((s) => (s = s - 1));
+    }, 1000);
+
+    return () => {
+      clearInterval(changePageTime);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (time <= 0) {
+      router.push('/');
+    }
+  }, [router, time]);
+  return (
+    <h1>PAGINA NÃO ENCONTRADA OU SEM POST retornando ao inicio em {time} </h1>
+  );
 }
