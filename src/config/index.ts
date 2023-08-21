1dae2c3ccd113cbe940cdec7f6af9e339c7b807a
Lord Ghapa <felipef32@hotmail.com>
2023-08-21 18:18:52 -0300

    create .env logic

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
+
 export default {
-  graphqlURL: 'http://127.0.0.1:1337/graphql',
+  graphqlURL: process?.env?.API_URL ?? 'http://127.0.0.1:1337/graphql',
   URL_SITE: 'localhost:3000',
 };
+
+console.log(`API Key: ${process.env.API_URL}`);
