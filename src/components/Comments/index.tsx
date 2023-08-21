aea649371943fc88619f1e0f66c642b63db904a6
Lord Ghapa <felipef32@hotmail.com>
2023-08-21 20:34:40 -0300

    update

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,5 +1,6 @@
 import * as Styled from './styles';
 import { DiscussionEmbed } from 'disqus-react';
+import config from '../../config/index';
 
 export type CommentsProps = {
   slug: string;
@@ -12,7 +13,7 @@ export const Comments = ({ slug, title }: CommentsProps) => {
       <DiscussionEmbed
         shortname="ia-historietas"
         config={{
-          url: `/post/${slug}`,
+          url: `${config.URL_SITE}/post/${slug}`,
           identifier: slug,
           title: title,
           language: 'pt_BR',
--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,5 @@ require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
 
 export default {
   graphqlURL: process?.env?.API_URL ?? 'http://127.0.0.1:1337/graphql',
-  URL_SITE: 'localhost:3000',
+  URL_SITE: process?.env?.SITE_URL ?? 'localhost:3000',
 };
